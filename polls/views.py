90a4f32d856c06cfd33141bbdb5b6b78bb21236b
itachi176 <lhoang17062000@gmail.com>
2021-08-20 15:04:09 +0700

    update question to admin

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("hoàng đẹp trai")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/base.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at the question: %s" % question_id)
+
+def results(request, question_id):
+    response = "you are looking at the question: %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
